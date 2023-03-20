0e5ba73ea128dcfbf80f2889a0343046733e9549
Carlos <carlosvaleriano45@yahoo.com>
2023-03-20 18:52:32 -0500

    Added game session state class

---
+++ RPG.Game.Engine/ViewModels/GameSession.cs
@@ -0,0 +1,34 @@
+﻿using RPG.Game.Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//View <-> ViewModel <-> Model
+
+namespace RPG.Game.Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+			this.CurrentPlayer = new Player
+			{
+				Name = "Flynn",
+				CharacterClass = "Samurai",
+				HitPoints = 10,
+				Gold = 1000,
+				ExperiencePoints = 0,
+				Level = 1
+			};
+		}
+
+        public void AddXP()
+        {
+            this.CurrentPlayer.ExperiencePoints += 10;
+        }
+    }
+}
